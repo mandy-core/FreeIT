5d01b38b1bc0d7f7cc539014207fbaad1f02241c
maxzeehazy <maxim.zh@outlook.com>
2022-07-27 19:10:44 +0200

    Create main.py

---
+++ Python/main.py
@@ -0,0 +1,44 @@
+import requests
+from datetime import datetime, timedelta
+
+##### Генератор списков
+url_name = 'https://api.exchangerate.host/symbols'
+response_name = requests.get(url_name)
+data_name = response_name.json()
+X_name = data_name.get('symbols')
+
+# Словарь со название валют
+list_name = {}
+for key, val in X_name.items():
+    X_dis = X_name.get(key)
+    list_name[key] = X_dis.get('description')
+
+
+###### Инфа за 30 дней
+Date = datetime.now().date()
+Thirty_days = Date - timedelta(days=30)
+url = f'https://api.exchangerate.host/fluctuation?start_date={Date}&end_date={Thirty_days}&base=EUR'
+response = requests.get(url)
+data = response.json()
+
+
+X = data.get("rates")
+
+# Словарь с инф о валютах
+list_cr = {}
+for key, val in X.items():
+    X_cr = X.get(key)
+    list_cr[key] = X.get(key)
+
+
+# Запись Названия валют в текстовый файл Валюта - курс
+with open('rate.txt', 'w') as out:
+    for key, val in list_cr.items():
+        X_key = X.get(key)
+        out.write('{} - {}\n'.format(key, X_key.get('end_rate')))
+
+# Запись Названия важных событий в лог
+with open('alert.log', 'w') as out:
+    for key, val in list_cr.items():
+        X_key = X.get(key)
+        out.write('Name: {} Change:{} Start_rate: {}\n'.format(key, X_key.get('change'), X_key.get('start_rate')))
